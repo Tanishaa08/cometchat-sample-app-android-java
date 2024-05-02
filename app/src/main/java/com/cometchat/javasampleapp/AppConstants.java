80c49d9c5e7a181dbec889ccdabd41eb3153bdbb
Tanishaa08 <97739347+Tanishaa08@users.noreply.github.com>
2024-05-02 16:34:53 +0530

    Update AppConstants.java

--- app/src/main/java/com/cometchat/javasampleapp/AppConstants.java
+++ app/src/main/java/com/cometchat/javasampleapp/AppConstants.java
@@ -2,10 +2,10 @@ package com.cometchat.javasampleapp;
 
 public class AppConstants {
 
-    public static final String APP_ID = "XXXXXXXXXXXXXXXXX";
+    public static final String APP_ID = "2569327ad452c12f";
 
-    public static final String AUTH_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_KEY = "29b507a80e5f33c10e05f57bb12da997742eb980";
 
-    public static final String REGION = "XX";
+    public static final String REGION = "in";
 
 }
